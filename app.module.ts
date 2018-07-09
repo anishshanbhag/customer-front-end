f503fa49703689ed06242a3ea0dbe66de62eb86a
Anish Shanbhag <anishshanbhag15@gmail.com>
2018-07-09 12:28:38 +0530

    pushing

---
+++ app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { LetterBoldPipe } from './shared/letter-bold.pipe';
+import { SearchFilterPipe } from './shared/filter-pipe';
+import { ClickOutsideDirective } from './shared/dropdown.directive';
+import { ApiService } from './shared';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ConnectionpageComponent } from './connectionpage/connectionpage.component';
+
+
+const appRoutes : Routes = [
+  {
+    path : '',
+    component : HomeComponent
+  },
+  {
+    path : 'connection',
+    component : ConnectionpageComponent
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ClickOutsideDirective,
+    SearchFilterPipe,
+    LetterBoldPipe,
+    ConnectionpageComponent
+    ],
+    imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
